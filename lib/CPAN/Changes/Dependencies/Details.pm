d864e40d2262985ec02cadf921b559cb1cd7a2d8
Kent Fredric <kentfredric@gmail.com>
2014-08-16 12:42:25 +1200

    Build results of b2e994d (on master)

--- lib/CPAN/Changes/Dependencies/Details.pm
+++ lib/CPAN/Changes/Dependencies/Details.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package CPAN::Changes::Dependencies::Details;
 
-our $VERSION = '0.001002';
+our $VERSION = '0.001003';
 
 # ABSTRACT: Create CPAN::Changes style file only containing dependency change information
 
@@ -88,7 +88,7 @@ CPAN::Changes::Dependencies::Details - Create CPAN::Changes style file only cont
 
 =head1 VERSION
 
-version 0.001002
+version 0.001003
 
 =head1 SYNOPSIS
 
